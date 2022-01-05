fe8976c9c6a4483015220dbdfc3a1c8def5c7b54
Shivan Kaul Sahib <shivankaulsahib@gmail.com>
2022-01-05 19:30:37 +0530

    De-amp feature
    Clean up regex and use re2 instead

--- browser/about_flags.cc
+++ browser/about_flags.cc
@@ -28,6 +28,7 @@
 #include "brave/components/skus/browser/switches.h"
 #include "brave/components/skus/common/features.h"
 #include "brave/components/speedreader/buildflags.h"
+#include "brave/components/de_amp/common/features.h"
 #include "brave/components/translate/core/common/brave_translate_features.h"
 #include "brave/components/translate/core/common/buildflags.h"
 #include "net/base/features.h"
@@ -67,6 +68,8 @@ using brave_shields::features::kBraveExtensionNetworkBlocking;
 using brave_shields::features::kCosmeticFilteringSyncLoad;
 
 using debounce::features::kBraveDebounce;
+using de_amp::features::kBraveDeAMP;
+
 using ntp_background_images::features::kBraveNTPBrandedWallpaperDemo;
 using ntp_background_images::features::kBraveNTPSuperReferralWallpaper;
 
@@ -145,6 +148,10 @@ constexpr char kBraveDomainBlock1PESDescription[] =
 constexpr char kBraveDebounceName[] = "Enable debouncing";
 constexpr char kBraveDebounceDescription[] =
     "Enable support for skipping top-level redirect tracking URLs";
+
+constexpr char kBraveDeAMPName[] = "Enable De-AMP";
+constexpr char kBraveDeAMPDescription[] =
+    "Enable De-AMPing feature";
 
 constexpr char kBraveExtensionNetworkBlockingName[] =
     "Enable extension network blocking";
@@ -504,6 +511,10 @@ const flags_ui::FeatureEntry::Choice kBraveSkusEnvChoices[] = {
         flag_descriptions::kBraveDebounceName,                              \
         flag_descriptions::kBraveDebounceDescription, kOsAll,               \
         FEATURE_VALUE_TYPE(kBraveDebounce)},                                \
+    {"brave-de-amp",                                                        \
+        flag_descriptions::kBraveDeAMPName,                                 \
+        flag_descriptions::kBraveDeAMPDescription, kOsAll,                  \
+        FEATURE_VALUE_TYPE(kBraveDeAMP)},                                   \
     {"brave-extension-network-blocking",                                    \
      flag_descriptions::kBraveExtensionNetworkBlockingName,                 \
      flag_descriptions::kBraveExtensionNetworkBlockingDescription, kOsAll,  \
